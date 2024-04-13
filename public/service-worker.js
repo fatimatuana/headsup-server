287ac0f84acec0d4f1553d1c24fc879fb49d2663
fatima tuana <fatima__tuana@hotmail.com>
2024-04-13 23:49:11 +0200

    logo/icons redesign

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.77fa555444c85af12a7ab302fd27160c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.95ace8ccc934745ae9528028a12100e4.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 if (workbox) {
