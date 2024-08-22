8ab7a2cd4a130991225e3e7715ac08fae3272a89
fatima tuana <fatima__tuana@hotmail.com>
2024-08-22 11:36:21 +0200

    handleorientation method is changed for bug fixing (audio)

---
+++ public/precache-manifest.cbc6c2f906a4a353ae489b3519649004.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f0e62e49703d8ab0b5",
+    "url": "/css/app.3ea461cb.css"
+  },
+  {
+    "revision": "df14ee4db33f369144c1",
+    "url": "/css/chunk-vendors.315b8b3e.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "6372890d9d26657f1d652f453ee66ef3",
+    "url": "/img/10.png"
+  },
+  {
+    "revision": "941ea1662a6da5c19271b425cdc746d5",
+    "url": "/img/11.png"
+  },
+  {
+    "revision": "45ad68e4e05266525f118eb7a9d19828",
+    "url": "/img/12.png"
+  },
+  {
+    "revision": "f9a5271135c4b77a16c882829b26b766",
+    "url": "/img/8.png"
+  },
+  {
+    "revision": "f3c4a935670e449affdfc5a42d63ae33",
+    "url": "/img/9.png"
+  },
+  {
+    "revision": "7b80ad20588fedb55c232f9009ea3aed",
+    "url": "/img/START.png"
+  },
+  {
+    "revision": "c2b10bdb9fce6020f7109dd27a1a1952",
+    "url": "/img/START_1.png"
+  },
+  {
+    "revision": "b35447933a622ee1f7bbc21c93126bb8",
+    "url": "/img/START_2.png"
+  },
+  {
+    "revision": "e622abe77cbfb88a5fcbc3c19f6ece54",
+    "url": "/img/START_3.png"
+  },
+  {
+    "revision": "0e2b9a4074b89ff9ff6f2ef016d16323",
+    "url": "/img/START_4.png"
+  },
+  {
+    "revision": "7a9fc22d7e90bd6968aaf38bb6b83b71",
+    "url": "/img/START_5.png"
+  },
+  {
+    "revision": "b6618c06bc347f63fe5e2c5a2ff0822a",
+    "url": "/img/START_7.png"
+  },
+  {
+    "revision": "8200acede0b375a4bc4b439dcec7b5bc",
+    "url": "/img/categories/celebrities.png"
+  },
+  {
+    "revision": "6c7f9a555b3a05301bc372769e910307",
+    "url": "/img/categories/countries.png"
+  },
+  {
+    "revision": "48f8a13ab3a576e26242c9f72202749b",
+    "url": "/img/categories/food.png"
+  },
+  {
+    "revision": "e9bf045604b73296c3373b246f758b79",
+    "url": "/img/categories/movies.png"
+  },
+  {
+    "revision": "20020f1f307921d35ec86e99f76e9f7a",
+    "url": "/img/categories/series.png"
+  },
+  {
+    "revision": "7fa51a433a034503152a6c92b79a52c2",
+    "url": "/img/categories/singers.png"
+  },
+  {
+    "revision": "e99f6c481315341f8dc3902cf7bbd106",
+    "url": "/img/categories/songs.png"
+  },
+  {
+    "revision": "8ff82d27beeb749edcd53972fe6d43c7",
+    "url": "/img/heads-up-bg-colors.png"
+  },
+  {
+    "revision": "2c7290cf011e636d003cadcee27b9f0e",
+    "url": "/img/heads-up-bg-logo.png"
+  },
+  {
+    "revision": "8e5ab0989d62839d65b50f3d54cec800",
+    "url": "/img/heads-up-bg.png"
+  },
+  {
+    "revision": "bf770a277454843e7f488b62a30993bd",
+    "url": "/img/heads-up-logo.png"
+  },
+  {
+    "revision": "75eef314dd75b1d62912f8fcdddbabc1",
+    "url": "/img/heads-up-title-ic.png"
+  },
+  {
+    "revision": "6106b248215226da70c33cb32148f6c4",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "af391703be8f19a3ec30a5971737175b",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "6106b248215226da70c33cb32148f6c4",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "af391703be8f19a3ec30a5971737175b",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "023d26be0f1f1e256feded45db8d4cc3",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "287d7a922970aff706ee4bdf6cbb2376",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "d3b4e93fea4350d2f472ef0cb2d39979",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9d648fdac66a4666abadcec054bcedbf",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "372c7ea3976b6239de7fd8e4810e1019",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "d3b4e93fea4350d2f472ef0cb2d39979",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "afa7d303c21b9602a18567cc4335be81",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "3ac453e87b832327437edd4b2817505d",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "d7814fcedcaf060689d128bbe7656790",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "0e76e6cd244e676a7314c7b0edded986",
+    "url": "/img/icons/logo.png"
+  },
+  {
+    "revision": "66898157b82cf05595ad79a3a8758853",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "f69667eee9423e795895b33e0b43dc61",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "d3cc30b9a78c6ba43dca2ddcd0429a83",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "d1a9812160be33233679bf76d77e7b2c",
+    "url": "/img/landing-page-bg.png"
+  },
+  {
+    "revision": "09bab17147bd44559c078cc2098e3334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21f0e62e49703d8ab0b5",
+    "url": "/js/app.c84fce97.js"
+  },
+  {
+    "revision": "df14ee4db33f369144c1",
+    "url": "/js/chunk-vendors.e8564beb.js"
+  },
+  {
+    "revision": "8aefdb21ab56e4e94c10adef1d879914",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "112084950c896cb5046a95e23b54a68a",
+    "url": "/media/lose.11208495.mp3"
+  },
+  {
+    "revision": "ecab699a5e6579b8b82558a8bd448cbd",
+    "url": "/media/win.ecab699a.mp3"
+  },
+  {
+    "revision": "0b53a5fc0e65f0da59a4f82b0a266e50",
+    "url": "/plain-speak-up.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2367d4aab1f836d5d3abd225f08465b9",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "112084950c896cb5046a95e23b54a68a",
+    "url": "/sounds/lose.mp3"
+  },
+  {
+    "revision": "ecab699a5e6579b8b82558a8bd448cbd",
+    "url": "/sounds/win.mp3"
+  },
+  {
+    "revision": "afa8e7ea57def96c7fd133fcdc54e970",
+    "url": "/speak-up.png"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.92731e5fffbe0798b634032a659cdf75.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.cbc6c2f906a4a353ae489b3519649004.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 if (workbox) {
